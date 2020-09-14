e5d561faea92cfc3ac73292a89178a537863d202
David Quintana <gigaherz@gmail.com>
2020-09-14 02:25:35 +0200

    More work on data checks.

--- buildSrc/src/main/java/dev/gigaherz/mappingdownloader/ConvertToCsvTask.java
+++ buildSrc/src/main/java/dev/gigaherz/mappingdownloader/ConvertToCsvTask.java
@@ -1,13 +1,13 @@
 package dev.gigaherz.mappingdownloader;
 
-import org.apache.commons.csv.CSVFormat;
+import com.google.common.collect.ArrayListMultimap;
+import com.google.common.collect.Multimap;
 import org.apache.commons.csv.CSVParser;
 import org.apache.commons.csv.CSVPrinter;
 import org.apache.commons.csv.CSVRecord;
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
 import org.gradle.api.DefaultTask;
-import org.gradle.api.internal.TaskOutputsInternal;
 import org.gradle.api.tasks.InputFiles;
 import org.gradle.api.tasks.OutputFiles;
 import org.gradle.api.tasks.TaskAction;
@@ -17,10 +17,7 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
-import java.util.Comparator;
 import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class ConvertToCsvTask extends DefaultTask
 {
@@ -30,27 +27,37 @@ public class ConvertToCsvTask extends DefaultTask
     public File destinationDir;
     public File inputFile;
 
-    public void inputFile(File file) {
+    public boolean checkDuplicates = false;
+
+    public void inputFile(File file)
+    {
         this.inputFile = file;
     }
 
-    public void setDestinationDir(File file) {
+    public void setDestinationDir(File file)
+    {
         destinationDir = file;
     }
-    public void destinationDir(File file) {
+
+    public void destinationDir(File file)
+    {
         setDestinationDir(file);
     }
-    public File getDestinationDir() {
+
+    public File getDestinationDir()
+    {
         return destinationDir;
     }
 
     @InputFiles
-    public List<File> getInputFiles() {
+    public List<File> getInputFiles()
+    {
         return Arrays.asList(inputFile);
     }
 
     @OutputFiles
-    public List<File> getOutputFiles() {
+    public List<File> getOutputFiles()
+    {
         File fieldsFile = new File(destinationDir, "fields.csv");
         File methodsFile = new File(destinationDir, "methods.csv");
         File paramsFile = new File(destinationDir, "params.csv");
@@ -63,50 +70,171 @@ public class ConvertToCsvTask extends DefaultTask
         if (!destinationDir.exists())
             destinationDir.mkdirs();
 
+        boolean anyErrors = false;
+
+        Multimap<String, String> classFieldCheck = ArrayListMultimap.create();
+        Multimap<String, String> classParamCheck = ArrayListMultimap.create();
         File fieldsFile = new File(destinationDir, "fields.csv");
         File methodsFile = new File(destinationDir, "methods.csv");
         File paramsFile = new File(destinationDir, "params.csv");
         try (
                 CSVParser parser = CSVParser.parse(inputFile, StandardCharsets.UTF_8, Utils.CUSTOM_CSV_FORMAT);
-                CSVPrinter fields = new CSVPrinter(new FileWriter(fieldsFile), Utils.CUSTOM_CSV_FORMAT);
-                CSVPrinter methods = new CSVPrinter(new FileWriter(methodsFile), Utils.CUSTOM_CSV_FORMAT);
-                CSVPrinter params = new CSVPrinter(new FileWriter(paramsFile), Utils.CUSTOM_CSV_FORMAT)
         )
         {
             List<CSVRecord> records = parser.getRecords();
-
-            fields.printRecord("searge","name","side","desc");
-            methods.printRecord("searge","name","side","desc");
-            params.printRecord("param","name","side");
-            for (CSVRecord record : records)
+            try (CSVPrinter fields = new CSVPrinter(new FileWriter(fieldsFile), Utils.CUSTOM_CSV_FORMAT))
             {
-                if (record.size() != 6)
-                    LOGGER.warn(String.format("Warning at entry, incorrect number of entries at: %s", record));
-                boolean include = ("TRUE".equals(record.get(0)) || includeUnverified)
-                        && !isNullOrEmpty(record.get(2)) && !isNullOrEmpty(record.get(3));
-                if (include)
+                fields.printRecord("searge", "name", "side", "desc");
+                for (CSVRecord record : records)
                 {
-                    String srgName = record.get(2);
-                    String mcpName = record.get(3);
-                    String side = "2"; // record.get(4); -- FIXME: Uncomment after testing
-                    String comment = record.get(5);
-                    if (side == null) side = "2";
+                    if (record.size() != 6)
+                        LOGGER.warn(String.format("Warning at entry, incorrect number of entries at: %s", record));
+                    boolean include = ("TRUE".equals(record.get(0)) || includeUnverified)
+                            && !isNullOrEmpty(record.get(2)) && !isNullOrEmpty(record.get(3));
+                    if (include)
+                    {
+                        String className = record.get(1);
+                        String srgName = record.get(2);
+                        String mcpName = record.get(3);
+                        String side = record.get(4);
+                        String comment = record.get(5);
+                        if (Utils.isNullOrEmpty(side)) side = "2";
 
-                    if (srgName.contains(" ")) LOGGER.error("SRG name contains spaces: " + srgName);
+                        if (srgName.contains(" "))
+                        {
+                            LOGGER.error("SRG name contains spaces: " + srgName);
+                            anyErrors = true;
+                            continue;
+                        }
 
-                    if (!Utils.LEGAL_NAME.matcher(mcpName).matches()) LOGGER.error("MCP name invalid: " + mcpName);
-                    if (side.contains(" ")) LOGGER.error("Side contains spaces: " + side);
-                    if (srgName.startsWith("field_"))
-                        fields.printRecord(srgName, mcpName, side, comment);
-                    else if (srgName.startsWith("func_"))
-                        methods.printRecord(srgName, mcpName, side, comment);
-                    else if (srgName.startsWith("p_"))
-                        params.printRecord(srgName, mcpName, side);
-                    else
-                        LOGGER.warn("Invalid SRG name: " + srgName);
+                        if (!Utils.LEGAL_NAME.matcher(mcpName).matches())
+                        {
+                            LOGGER.error("MCP name invalid: " + mcpName);
+                            anyErrors = true;
+                            continue;
+                        }
+                        if (side.contains(" "))
+                        {
+                            LOGGER.error("Side contains spaces: " + side);
+                            anyErrors = true;
+                            side = "2";
+                        }
+                        if (srgName.startsWith("field_"))
+                        {
+                            fields.printRecord(srgName, mcpName, side, comment);
+
+                            if (checkDuplicates && classFieldCheck.containsEntry(className, mcpName))
+                            {
+                                LOGGER.error(String.format("A field already exists with the same name in this class: %s,%s,%s", srgName, mcpName, side));
+                                anyErrors = true;
+                                continue;
+                            }
+                            classFieldCheck.put(className, mcpName);
+                        }
+                    }
+                }
+            }
+            try (CSVPrinter methods = new CSVPrinter(new FileWriter(methodsFile), Utils.CUSTOM_CSV_FORMAT))
+            {
+                methods.printRecord("searge", "name", "side", "desc");
+                for (CSVRecord record : records)
+                {
+                    if (record.size() != 6)
+                        LOGGER.warn(String.format("Warning at entry, incorrect number of entries at: %s", record));
+                    boolean include = ("TRUE".equals(record.get(0)) || includeUnverified)
+                            && !isNullOrEmpty(record.get(2)) && !isNullOrEmpty(record.get(3));
+                    if (include)
+                    {
+                        String className = record.get(1);
+                        String srgName = record.get(2);
+                        String mcpName = record.get(3);
+                        String side = record.get(4);
+                        String comment = record.get(5);
+                        if (Utils.isNullOrEmpty(side)) side = "2";
+
+                        if (srgName.contains(" "))
+                        {
+                            LOGGER.error("SRG name contains spaces: " + srgName);
+                            anyErrors = true;
+                            continue;
+                        }
+
+                        if (!Utils.LEGAL_NAME.matcher(mcpName).matches())
+                        {
+                            LOGGER.error("MCP name invalid: " + mcpName);
+                            anyErrors = true;
+                            continue;
+                        }
+                        if (side.contains(" "))
+                        {
+                            LOGGER.error("Side contains spaces: " + side);
+                            anyErrors = true;
+                            side = "2";
+                        }
+                        if (srgName.startsWith("func_"))
+                        {
+                            methods.printRecord(srgName, mcpName, side, comment);
+                        }
+                    }
+                }
+            }
+            try (CSVPrinter params = new CSVPrinter(new FileWriter(paramsFile), Utils.CUSTOM_CSV_FORMAT))
+            {
+                params.printRecord("param", "name", "side");
+                for (CSVRecord record : records)
+                {
+                    if (record.size() != 6)
+                        LOGGER.warn(String.format("Warning at entry, incorrect number of entries at: %s", record));
+                    boolean include = ("TRUE".equals(record.get(0)) || includeUnverified)
+                            && !isNullOrEmpty(record.get(2)) && !isNullOrEmpty(record.get(3));
+                    if (include)
+                    {
+                        String className = record.get(1);
+                        String srgName = record.get(2);
+                        String mcpName = record.get(3);
+                        String side = record.get(4);
+                        String comment = record.get(5);
+                        if (Utils.isNullOrEmpty(side)) side = "2";
+
+                        if (srgName.contains(" "))
+                        {
+                            LOGGER.error("SRG name contains spaces: " + srgName);
+                            anyErrors = true;
+                            continue;
+                        }
+
+                        if (!Utils.LEGAL_NAME.matcher(mcpName).matches())
+                        {
+                            LOGGER.error("MCP name invalid: " + mcpName);
+                            anyErrors = true;
+                            continue;
+                        }
+
+                        if (side.contains(" "))
+                        {
+                            LOGGER.error("Side contains spaces: " + side);
+                            anyErrors = true;
+                            side = "2";
+                        }
+
+                        if (srgName.startsWith("p_"))
+                        {
+                            params.printRecord(srgName, mcpName, side);
+                            if (checkDuplicates && classFieldCheck.containsEntry(className, mcpName))
+                            {
+                                LOGGER.error(String.format("A param exists with the same name as a field: %s,%s,%s", srgName,mcpName,side));
+                                anyErrors = true;
+                                continue;
+                            }
+                            classParamCheck.put(className, mcpName);
+                        }
+                    }
                 }
             }
         }
+
+        if (anyErrors)
+            throw new RuntimeException("Finished with errors!");
     }
 
     private static boolean isNullOrEmpty(String s)
